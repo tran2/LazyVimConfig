b81be99d13159a1abca2f7050187b6677505b057
Tran Tu <tran.tu@nutanix.com>
2023-07-21 23:10:22 -0700

    add copy file path

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
 -- -- Disable autoformat for lua files
+-- add a file autoformat-ignore with content like {
+--   "**/node_modules/*",
+-- }
 vim.api.nvim_create_autocmd({ "BufRead" }, {
   pattern = require("config.autoformat-ignore"),
   callback = function()
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps her
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape" })
+
+function CopyCurrentBufferPath()
+  local currentBuff = vim.api.nvim_get_current_buf()
+  local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(currentBuff), ":p:.")
+
+  vim.fn.setreg("+", fname)
+end
+
+vim.keymap.set("n", "<leader>bac", "<cmd>:lua CopyCurrentBufferPath()<CR>", { desc = "Copy current file path" })
