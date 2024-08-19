202bddf9cc9951c1e158f9400574a212164c019a
Tran Tu <tran.tu@nutanix.com>
2024-08-19 14:24:33 -0700

    fix switcher and add storybook switch

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,24 +1,40 @@
+function Quick_switcher_find_jsFileOnly()
+  -- use -vE for regex to inverse match so that it only matches Component.ts and not .test.ts files
+  -- this is because mac grep does not have -P for perl regex
+  require("nvim-quick-switcher").find(
+    "\\.test\\.|\\.stories.\\|\\.spec\\.",
+    { regex = true, prefix = "short", regex_type = "vE" }
+  )
+end
+
 return {
   {
     "Everduin94/nvim-quick-switcher",
     keys = {
       {
-        "<leader>js",
+        "<leader>jc",
         "<cmd>:lua require('nvim-quick-switcher').find('.+css|.+scss|.+sass|.+less', { regex = true, prefix='short' })<CR>",
         desc = "switch to style",
         noremap = true,
         silent = true,
       },
       {
+        "<leader>js",
+        "<cmd>:lua require('nvim-quick-switcher').find('(.+stories).(jsx|js)$', { regex = true, prefix='short' })<CR>",
+        desc = "switch to style",
+        noremap = true,
+        silent = true,
+      },
+      {
         "<leader>jt",
-        "<cmd>:lua require('nvim-quick-switcher').find('(.+test|.+spec).(jsx|js)$', { regex = true, prefix='full' })<CR>",
+        "<cmd>:lua require('nvim-quick-switcher').find('(.+test|.+spec).(jsx|js)$', { regex = true, prefix='short' })<CR>",
         desc = "switch to test or spec file",
         noremap = true,
         silent = true,
       },
       {
         "<leader>jj",
-        "<cmd>:lua require('nvim-quick-switcher').find('.(js|jsx|ts|tsx)$', { regex = true, prefix='short' })<CR>",
+        "<cmd>:lua Quick_switcher_find_jsFileOnly()<CR>",
         desc = "switch to jsx or js",
         noremap = true,
         silent = true,
